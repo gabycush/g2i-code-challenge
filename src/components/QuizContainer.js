0b8e49b622c401544b1cc40310701b7b407f1528
Gaby Cushenan <gabriela.c@vibenomics.com>
2022-08-31 12:38:53 -0400

    naming updates

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -7,7 +7,7 @@ import { fetchQuestions } from "../store/questions";
 import { addResult } from "../store/results";
 
 const QuizContainer = () => {
-  const [qIndex, setQIndex] = useState(1);
+  const [questionIndex, setQuestionIndex] = useState(1);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchQuestions());
@@ -16,45 +16,45 @@ const QuizContainer = () => {
   const { questions, isLoading } = useSelector((state) => state.questions);
 
   const questionCount = isLoading ? 0 : questions.length;
-  const currentQ = questions[qIndex - 1];
-  const quizComplete = qIndex > questionCount;
+  const current = questions[questionIndex - 1];
 
   const answerQuestion = (value) => {
-    const gradedQ = {
-      question: he.decode(currentQ.question),
-      answer: value,
-      correct: value === currentQ.correct_answer,
+    const graded = {
+      question: he.decode(current.question),
+      correct: value === current.correct_answer,
     };
 
-    dispatch(addResult(gradedQ));
-    setQIndex(qIndex + 1);
+    dispatch(addResult(graded));
+    setQuestionIndex(questionIndex + 1);
   };
 
   return (
     <Container maxWidth="sm">
       <Box className="box-sm category">
-        <h3>{isLoading ? "" : currentQ?.category}</h3>
+        <h3>{isLoading ? "" : current?.category}</h3>
       </Box>
 
       <Box className="box-md">
-        {!quizComplete && (
-          <p className="question">
-            {isLoading ? "" : he.decode(currentQ.question)}
-          </p>
-        )}
+        <p className="question">
+          {isLoading ? "" : he.decode(current?.question)}
+        </p>
       </Box>
       <Box className="box-sm sub">
         <p>
-          {qIndex} of {questionCount}
+          {questionIndex} of {questionCount}
         </p>
       </Box>
       <Box className="box-sm button">
         <Box display="flex">
           <Button variant="contained" onClick={() => answerQuestion("True")}>
-            {qIndex >= questionCount ? <Link to="/results">True</Link> : "True"}
+            {questionIndex >= questionCount ? (
+              <Link to="/results">True</Link>
+            ) : (
+              "True"
+            )}
           </Button>
           <Button variant="contained" onClick={() => answerQuestion("False")}>
-            {qIndex >= questionCount ? (
+            {questionIndex >= questionCount ? (
               <Link to="/results">False</Link>
             ) : (
               "False"
